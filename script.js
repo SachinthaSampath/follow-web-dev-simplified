c45b64877cb5d1fe3eb2ee5f068d30f5821766fd
Sachintha Sampath <sachinsampath42@gmail.com>
2024-07-19 00:45:14 +0530

    feat: starting-style-animation

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const body = document.querySelector(".body")
 button.addEventListener("click", () => {
     body.classList.toggle("show")
 })
+
+
+const boxButton = document.querySelector(".box-button")
+const boxDiv = document.querySelector(".box-div")
+
+boxButton.addEventListener("click", () => {
+    boxDiv.classList.toggle("show")
+})
